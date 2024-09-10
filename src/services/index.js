77548939ab5d16bbafbcc0ca8d4b42651706dbd0
Icaro-Vieira <icarogbov@gmail.com>
2024-09-10 03:40:52 -0300

    Adicionando nova rota da API

--- src/services/index.js
+++ src/services/index.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem("token");
 
 export const atlantidaApi = axios.create({
-  // baseURL: 'http://ec2-54-236-98-121.compute-1.amazonaws.com:3000/api',
-  // baseURL: 'http://localhost:3000/api',
-  baseURL: 'https://atlantida-api-new.onrender.com/api',
+  baseURL: 'https://atlanticamergulhos-aqgvcwhwg5bjdpff.brazilsouth-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${token}`
