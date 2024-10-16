b5468d0100735a393f7b09d952caff5108725cd6
icaro vieira <78177376+Icaro-Vieira@users.noreply.github.com>
2024-10-15 21:42:46 -0300

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-const token = localStorage.getItem("token");
-
 export const atlantidaApi = axios.create({
   baseURL: 'http://201.23.16.247:3000/api',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`
   }
 });
