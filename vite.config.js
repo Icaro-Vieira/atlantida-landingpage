0c7e165914665ca73875da74ad4afff3bd0c1ec1
icaro vieira <78177376+Icaro-Vieira@users.noreply.github.com>
2024-10-15 22:20:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, 
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Servidor da API local
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', // Diretório onde o Vite irá gerar os arquivos de produção
+    sourcemap: true, // Gera mapas de origem para facilitar o debug em produção
+  },
+  resolve: {
+    alias: {
+      // Definir alias para facilitar a importação de componentes e arquivos
+      '@': '/src',
+    },
+  },
+  define: {
+    'process.env': {}, // Define variáveis de ambiente (se necessário)
+  },
 })
