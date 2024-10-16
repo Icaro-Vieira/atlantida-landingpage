9f7b48a836f87212de48ad6c3eadd88f7a701924
icaro vieira <78177376+Icaro-Vieira@users.noreply.github.com>
2024-10-15 21:55:42 -0300

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export const atlantidaApi = axios.create({
-  baseURL: 'http://201.23.16.247:3000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  }
+  baseURL: 'http://201.23.16.247:3000/api'
 });
